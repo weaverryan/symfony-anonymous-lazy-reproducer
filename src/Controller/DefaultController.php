63e5039ab07965d363f624670f67cdf5613e0006
Ryan Weaver <ryan@thatsquality.com>
2019-11-25 16:35:52 -0500

    authenticator setup

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController
 {
     public function index()
     {
-        return new Response('Should not be hit - AppCustomAuthenticator::supports() has a die in it');
+        return new Response('Homepage! Go to /login');
     }
 }
